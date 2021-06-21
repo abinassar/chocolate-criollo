122858c9e80dfb526ec7c93f8840db74ad5181a4
abinassar <abinassar@gmail.com>
2021-06-21 16:32:52 -0400

    Agregadas tarjetas con chocolates

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -7,6 +7,32 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
+  productos: any = [
+    {
+      nombre: 'Chocolate crema de Maní y Leche'
+    },
+    {
+      nombre: 'Chocolate y Ron 70%'
+    },
+    {
+      nombre: 'Chocolate Oscuro 61.9%'
+    },
+    {
+      nombre: 'Chocolate Leche y Café'
+    },
+    {
+      nombre: 'Chocolate Leche y Menta'
+    },
+    {
+      nombre: 'Chocolate Dulce de Leche'
+    },
+    {
+      nombre: 'Chocolate Leche y Galleta'
+    }
+]
+
+
+
   constructor() {}
 
 }
