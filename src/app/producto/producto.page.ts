7b140574eb84e22ca48d718bdaa039dbfe06d890
abinassar <abinassar@gmail.com>
2021-07-01 08:32:44 -0400

    modificación de pagina de producto

--- src/app/producto/producto.page.ts
+++ src/app/producto/producto.page.ts
@@ -11,6 +11,11 @@ export class ProductoPage implements OnInit {
   producto: any;
   productos: any;
 
+  slideOpts = {
+    initialSlide: 1,
+    speed: 400
+  };
+
   constructor( private _dataService: DataService) {
     this.productos = this._dataService.setData();
     this.producto = this.productos[this._dataService.productoSeleccionado];
