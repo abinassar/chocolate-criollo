a8a5b0aeeef3c2cd8f4cd283714ee5df292165fe
abinassar <abinassar@gmail.com>
2021-07-26 10:55:29 -0400

    Agregados slides debajo de la descripción del producto

--- src/app/producto/producto.page.ts
+++ src/app/producto/producto.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ToastController } from '@ionic/angular';
 import { DataService } from '../services/data-service.service';
 
 @Component({
@@ -10,6 +11,7 @@ export class ProductoPage implements OnInit {
 
   producto: any;
   productos: any;
+  cantidadProducto: number = 10;
 
   slideOpts = {
     initialSlide: 1,
@@ -18,16 +20,33 @@ export class ProductoPage implements OnInit {
 
   
 
-  constructor( private _dataService: DataService) {
+  constructor( private _dataService: DataService,
+               public _toastController: ToastController) {}
+
+  ngOnInit() {
     this.productos = this._dataService.setData();
-    console.log(this.producto)
+    console.log(this.producto);
     if (this.producto) {
       this.producto = this.productos[this._dataService.productoSeleccionado];
     }
     this.producto = this.productos[1];
-   }
-
-  ngOnInit() {
   }
+
+  increaseQuantity() {
+    this.cantidadProducto += 1;
+  }
+
+  decreaseQuantity() {
+    this.cantidadProducto -= 1;
+  }
+
+    async orderProduct() {
+      const toast = await this._toastController.create({
+        message: 'Producto Agregado!',
+        duration: 2000,
+        color: "success"
+      });
+      toast.present();
+    }
 
 }
--- src/app/services/data-service.service.ts
+++ src/app/services/data-service.service.ts
@@ -8,55 +8,68 @@ export class DataService {
   productos = [
     {
       nombre: 'Chocolate crema de Maní y Leche',
-      img: '../../assets/images/Chocolate-Leche-y-Mani.jpg.png'
+      img: '../../assets/images/Chocolate-Leche-y-Mani.jpg.png',
+      mensaje: 'Lo más delicioso del maní se hace presente, esto junto con la dulzura de la leche envuelta en un chocolate, eso representa el chocolate de Maní y Leche, no te lo pierdas!.'
     },
     {
       nombre: 'Chocolate y Ron 70%',
-      img: '../../assets/images/Chocolate-Ron-65.jpg.png'
+      img: '../../assets/images/Chocolate-Ron-65.jpg.png',
+      mensaje: 'Venezuela se distingue por su increíble naturaleza tropical, su gente, lo exquisito de su ron y lo divino de su cacao considerado el mejor cacao del mundo, un trocito de Venezuela en un cacao.'
     },
     {
       nombre: 'Chocolate Oscuro 61.9%',
-      img: '../../assets/images/Chocolate-61.9-Cacao.jpg.png'
+      img: '../../assets/images/Chocolate-61.9-Cacao.jpg.png',
+      mensaje: 'Este chocolate nos encanta por su sabor equilibrado del cacao, hecho para los que quieren los beneficios del cacao sin perder su dulzura.'
     },
     {
       nombre: 'Chocolate Leche y Café',
-      img: '../../assets/images/Chocolate-Leche-y-Cafe.jpg.png'
+      img: '../../assets/images/Chocolate-Leche-y-Cafe.jpg.png',
+      mensaje: 'Café de nuestro origen esto marca la diferencia trocitos de café tostado lo delicioso del chocolate lo cremoso de la leche y los aromas del café, un chocolate para competir.'
     },
     {
       nombre: 'Chocolate Leche y Menta',
-      img: '../../assets/images/Chocolate-Leche-y-Menta.jpg.png'
+      img: '../../assets/images/Chocolate-Leche-y-Menta.jpg.png',
+      mensaje: 'Una versión diferente, un chocolate para disfrutarlo con todos los sentidos, la sensualidad hecha chocolate.'
     },
     {
       nombre: 'Chocolate Dulce de Leche',
-      img: '../../assets/images/Chocolate-Dulce-de-Leche.jpg.png'
+      img: '../../assets/images/Chocolate-Dulce-de-Leche.jpg.png',
+      mensaje: 'Aquí comienza la magia de un chocolate, la combinación del dulce de leche bañado en chocolate oscuro buscado la intensidad del chocolate y lo subyacente del dulzor.'
     },
     {
       nombre: 'Chocolate Leche y Galleta',
-      img: '../../assets/images/Chocolate-Leche-y-Galletas.jpg.png'
+      img: '../../assets/images/Chocolate-Leche-y-Galletas.jpg.png',
+      mensaje: 'Nuestras crujientes galletas caseras, crean una divertida y deliciosa experiencia de un crunchy que querrás repetir.'
     },
     {
       nombre: 'Chocolate Mango',
-      img: '../../assets/images/Chocolate-Mango.jpg.png'
+      img: '../../assets/images/Chocolate-Mango.jpg.png',
+      mensaje: 'La mezcla del sabor de un buen Chocolate junto al dulce del mango son la formula perfecta que componen este chocolate.'
     },
     {
       nombre: 'Chocolate de Parchita y Piña',
-      img: '../../assets/images/Chocolate-Parchita-y-Pina.jpg.png'
+      img: '../../assets/images/Chocolate-Parchita-y-Pina.jpg.png',
+      mensaje: 'Para los amantes del ácido, llega la unión del cítrico con el placer del cacao en este chocolate de parchita y piña.'
     },
     {
       nombre: 'Chocolate Pie de Limón',
-      img: '../../assets/images/Chocolate-Pie-de-Limon.jpg.png'
+      img: '../../assets/images/Chocolate-Pie-de-Limon.jpg.png',
+      mensaje: 'El increíble saber de una buena trata de limón, traída en una tableta de chocolate, ¡no dejes de probarla! de seguro te encantará.'
     },
     {
       nombre: 'Chocolate Tres Leches',
-      img: '../../assets/images/Chocolate-Tres-Leches.jpg.png'
+      img: '../../assets/images/Chocolate-Tres-Leches.jpg.png',
+      mensaje: 'Este chocolate trae lo mejor de dos ricos postres: el pastel tres leches y el sabor de un buen chocolate. Una verdadera delicia, no puedes dejar de probarlo.'
     },
     {
       nombre: 'Chocolate Naranja y Galletas',
-      img: '../../assets/images/Chocolate-Trocitos-de-Naranjas-y-Galletas.jpg.png'
+      img: '../../assets/images/Chocolate-Trocitos-de-Naranjas-y-Galletas.jpg.png',
+      mensaje: 'Una verdadera fiesta de sabores almacenada en una tableta de chocolate, ideal para los amantes de la variedad. Despierta tus sentidos y pruébalo ya!.'
     },
     {
       nombre: 'Rocas de Chocolate',
-      img: '../../assets/images/Rocas-De-Chocolate.png'
+      img: '../../assets/images/Rocas-De-Chocolate.png',
+      mensaje: 'Alegra tu día con esta dulzura hecha chocolate, práctico para llevarlo donde quieras. Definitivamente las rocas de chocolate están hechas para tí!.'
     }
 ]
 
